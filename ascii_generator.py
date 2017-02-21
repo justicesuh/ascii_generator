021cd5ce309ba8a15fe74be72f8ddfa998f78c99
Justice Suh <justice.suh@gmail.com>
2017-02-20 23:34:05 -0500

    Create empty dot matrix

--- ascii_generator.py
+++ ascii_generator.py
@@ -3,18 +3,22 @@ from __future__ import print_function
 from PIL import Image
 
 if __name__ == '__main__':
-    ascii_table = Image.open('ascii.bmp')
     start_width = 76
     start_height = 26
     tile_size = 24
-    ascii_code = 32
-    
+    tiles = []
+
+    # partition ascii table
+    ascii_table = Image.open('ascii.bmp') 
     for i in range(6):
         for k in range(16):
             width = start_width + i * (tile_size + 1)
             height = start_height + k * (tile_size + 1)
             tile = ascii_table.crop((width, height, width + tile_size, height + tile_size))
-            tile.save('ascii{}.jpg'.format(ascii_code))
-            ascii_code += 1
+            tiles.append(tile)
 
-
+    # convert from noritake itron format to 5x7 format
+    ascii_offset = 32
+    for i in range(len(tiles)):
+        dot_matrix = Image.new('RGB', (7, 9), (255, 255, 255))
+#        tiles[i].save('ascii{}.jpg'.format(i + ascii_offset))
